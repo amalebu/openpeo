e04be542f86bdb2b5854de0fad18e46ae389515d
Ama Lebu <66108651+amalebu@users.noreply.github.com>
2024-09-27 16:08:42 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/openpeo',
+  base: 'https://amalebu.github.io/openpeo/',
   resolve: {
     alias: {
       '@' : path.resolve(__dirname, './src')
